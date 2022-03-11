23d4e78c024c5d76589bfe9f0011675583bc3002
mbrugnoni <michael.brugnoni@gmail.com>
2022-03-10 21:43:34 -0500

    Ansible finally working on the pass off

--- main.tf
+++ main.tf
@@ -9,45 +9,49 @@ terraform {
 provider "proxmox" {
   pm_api_url = "https://192.168.50.72:8006/api2/json"
   # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = "api_user@pam!api-token-1"
+  pm_api_token_id     = "api_user@pam!api-token-1"
   pm_api_token_secret = "6bdd0a35-bc4a-4b81-aef2-3388ec026fc0"
-  pm_tls_insecure = true
+  pm_tls_insecure     = true
 }
 
 resource "proxmox_vm_qemu" "minecraft1" {
-  count = 1
-  name = "minecraft${count.index + 1}"
+  count       = 1
+  name        = "minecraft${count.index + 1}"
   target_node = var.proxmox_host
-  clone = var.template_name
-  agent = 1
-  os_type = "cloud-init"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 4096
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
+  clone       = var.template_name
+  agent       = 1
+  os_type     = "cloud-init"
+  cores       = 2
+  sockets     = 1
+  cpu         = "host"
+  memory      = 4096
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
   disk {
-    slot = 0
-    size = "10G"
-    type = "scsi"
-    storage = "local-lvm"
+    slot     = 0
+    size     = "10G"
+    type     = "scsi"
+    storage  = "local-lvm"
     iothread = 1
   }
-  
+
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr0"
   }
 
   ipconfig0 = "ip=dhcp"
-  
+
   sshkeys = <<EOF
   ${var.ssh_key}
   EOF
 }
 
-resource "local_file" "tf_ansible_vars" {
-  content = proxmox_vm_qemu.minecraft1[0].default_ipv4_address
+resource "local_file" "tf_ansible_hosts" {
+  content  = proxmox_vm_qemu.minecraft1[0].default_ipv4_address
   filename = "./tf_ansible_hosts"
+  provisioner "local-exec" {
+    command = "sleep 90;export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${self.filename} minecraft_server_config.yml"
+  }
 }
+
